6616f655a7533ed42d66aad5d23667a6c2f22e0a
iamcosmin <47219753+iamcosmin@users.noreply.github.com>
2021-07-29 13:40:08 +0300

    Start

--- lib/interface/home/chat/chat.dart
+++ lib/interface/home/chat/chat.dart
@@ -1,3 +1,4 @@
+import 'package:allo/components/firestore_animated_list/animated_firestore_list.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:allo/components/progress_rings.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -21,13 +22,6 @@ class Chat extends HookWidget {
   Widget build(BuildContext context) {
     final scrollController = useScrollController();
     final documentLoad = useState(15);
-    final stream = FirebaseFirestore.instance
-        .collection('chats')
-        .doc(chatId)
-        .collection('messages')
-        .orderBy('time', descending: true)
-        .limit(documentLoad.value)
-        .snapshots();
     useEffect(() {
       if (!kIsWeb) {
         FirebaseMessaging.instance.subscribeToTopic(chatId);
@@ -66,79 +60,123 @@ class Chat extends HookWidget {
           child: Column(
             children: [
               Expanded(
-                flex: 1,
-                child: StreamBuilder(
-                  stream: stream,
-                  builder: (ctx, AsyncSnapshot<QuerySnapshot> snap) {
-                    return AnimatedSwitcher(
-                      duration: Duration(milliseconds: 500),
-                      switchInCurve: Curves.easeInCubic,
-                      switchOutCurve: Curves.easeOutCubic,
-                      child: snap.hasData
-                          ? NotificationListener(
-                              onNotification: (value) {
-                                if (value is ScrollNotification) {
-                                  final before = value.metrics.extentBefore;
-                                  final max = value.metrics.maxScrollExtent;
+                  flex: 1,
+                  child: FirestoreAnimatedList(
+                    query: FirebaseFirestore.instance
+                        .collection('chats')
+                        .doc(chatId)
+                        .collection('messages')
+                        .orderBy('time', descending: true)
+                        .limit(documentLoad.value),
+                    reverse: true,
+                    linear: false,
+                    duration: Duration(milliseconds: 150),
+                    itemBuilder: (context, snap, animation, i) {
+                      final nowData, pastData, nextData;
+                      nowData = snap![i]!.data() as Map;
+                      if (i == 0) {
+                        nextData = {'senderUID': 'null'};
+                      } else {
+                        nextData = snap[i - 1]!.data() as Map;
+                      }
+                      if (i == snap.length - 1) {
+                        pastData = {'senderUID': 'null'};
+                      } else {
+                        pastData = snap[i + 1]!.data() as Map;
+                      }
+                      // Above, pastData should have been i-1 and nextData i+1.
+                      // But, as the list needs to be in reverse order, we need
+                      // to consider this workaround.
+                      final pastUID = pastData.containsKey('senderUID')
+                          ? pastData['senderUID']
+                          : 'null';
+                      final nextUID = nextData.containsKey('senderUID')
+                          ? nextData['senderUID']
+                          : 'null';
+                      return SlideTransition(
+                        position: Tween<Offset>(
+                                begin: Offset(0, 1.0), end: Offset(0, 0))
+                            .animate(animation),
+                        child: MessageBubble(
+                          documentData: nowData,
+                          pastUID: pastUID,
+                          nextUID: nextUID,
+                        ),
+                      );
+                    },
+                  )
+                  // StreamBuilder(
+                  //   stream: stream,
+                  //   builder: (ctx, AsyncSnapshot<QuerySnapshot> snap) {
+                  //     return AnimatedSwitcher(
+                  //       duration: Duration(milliseconds: 500),
+                  //       switchInCurve: Curves.easeInCubic,
+                  //       switchOutCurve: Curves.easeOutCubic,
+                  //       child: snap.hasData
+                  //           ? NotificationListener(
+                  //               onNotification: (value) {
+                  //                 if (value is ScrollNotification) {
+                  //                   final before = value.metrics.extentBefore;
+                  //                   final max = value.metrics.maxScrollExtent;
 
-                                  if (before == max) {
-                                    documentLoad.value =
-                                        documentLoad.value + 15;
-                                  }
-                                }
-                                return false;
-                              },
-                              child: ListView.builder(
-                                reverse: true,
-                                controller: scrollController,
-                                itemCount: snap.data!.docs.length,
-                                itemBuilder: (BuildContext ctx, int i) {
-                                  final nowData, pastData, nextData;
-                                  nowData = snap.data!.docs[i].data() as Map;
-                                  if (i == 0) {
-                                    nextData = {'senderUID': 'null'};
-                                  } else {
-                                    nextData =
-                                        snap.data!.docs[i - 1].data() as Map;
-                                  }
-                                  if (i == snap.data!.docs.length - 1) {
-                                    pastData = {'senderUID': 'null'};
-                                  } else {
-                                    pastData =
-                                        snap.data!.docs[i + 1].data() as Map;
-                                  }
-                                  // Above, pastData should have been i-1 and nextData i+1.
-                                  // But, as the list needs to be in reverse order, we need
-                                  // to consider this workaround.
-                                  final pastUID =
-                                      pastData.containsKey('senderUID')
-                                          ? pastData['senderUID']
-                                          : 'null';
-                                  final nextUID =
-                                      nextData.containsKey('senderUID')
-                                          ? nextData['senderUID']
-                                          : 'null';
-                                  return MessageBubble(
-                                    documentData: nowData,
-                                    pastUID: pastUID,
-                                    nextUID: nextUID,
-                                  );
-                                },
-                              ),
-                            )
-                          : SafeArea(
-                              child: Center(
-                                child: Container(
-                                  height: 50,
-                                  width: 50,
-                                  child: ProgressRing(),
-                                ),
-                              ),
-                            ),
-                    );
-                  },
-                ),
-              ),
+                  //                   if (before == max) {
+                  //                     documentLoad.value =
+                  //                         documentLoad.value + 15;
+                  //                   }
+                  //                 }
+                  //                 return false;
+                  //               },
+                  //               child: ListView.builder(
+                  //                 reverse: true,
+                  //                 controller: scrollController,
+                  //                 itemCount: snap.data!.docs.length,
+                  //                 itemBuilder: (BuildContext ctx, int i) {
+                  //                   final nowData, pastData, nextData;
+                  //                   nowData = snap.data!.docs[i].data() as Map;
+                  //                   if (i == 0) {
+                  //                     nextData = {'senderUID': 'null'};
+                  //                   } else {
+                  //                     nextData =
+                  //                         snap.data!.docs[i - 1].data() as Map;
+                  //                   }
+                  //                   if (i == snap.data!.docs.length - 1) {
+                  //                     pastData = {'senderUID': 'null'};
+                  //                   } else {
+                  //                     pastData =
+                  //                         snap.data!.docs[i + 1].data() as Map;
+                  //                   }
+                  //                   // Above, pastData should have been i-1 and nextData i+1.
+                  //                   // But, as the list needs to be in reverse order, we need
+                  //                   // to consider this workaround.
+                  //                   final pastUID =
+                  //                       pastData.containsKey('senderUID')
+                  //                           ? pastData['senderUID']
+                  //                           : 'null';
+                  //                   final nextUID =
+                  //                       nextData.containsKey('senderUID')
+                  //                           ? nextData['senderUID']
+                  //                           : 'null';
+                  //                   return MessageBubble(
+                  //                     documentData: nowData,
+                  //                     pastUID: pastUID,
+                  //                     nextUID: nextUID,
+                  //                   );
+                  //                 },
+                  //               ),
+                  //             )
+                  //           : SafeArea(
+                  //               child: Center(
+                  //                 child: Container(
+                  //                   height: 50,
+                  //                   width: 50,
+                  //                   child: ProgressRing(),
+                  //                 ),
+                  //               ),
+                  //             ),
+                  //     );
+                  //   },
+                  // ),
+                  ),
               Expanded(
                 flex: 0,
                 child: Align(
