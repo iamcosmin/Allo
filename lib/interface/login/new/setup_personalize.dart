db3b60bcc50fb73fb4f121f4fab4bd133423600c
iamcosmin <47219753+iamcosmin@users.noreply.github.com>
2022-06-07 21:11:59 +0300

    1.3.4 (291+1)
    - fixed an issue which threw the compiler off.
    
    From 1.3.4 (291):
    - update splash on Android 12 and above (animated splash)
    - replaced PreferenceManager with SettingManager
    - the new Switch is now adaptive (it will use the iOS switch on iOS)
    - fixed typography: same font, all platforms, iOS will use its own SF font
    - now, dark mode is system by default. Change it in settings.
    - adaptive icons for web
    - new branding
    - updated web, many improvements

--- lib/interface/login/new/setup_personalize.dart
+++ lib/interface/login/new/setup_personalize.dart
@@ -1,6 +1,6 @@
 import 'package:allo/components/setup_page.dart';
 import 'package:allo/interface/login/new/setup_done.dart';
-import 'package:allo/logic/client/hooks.dart';
+import 'package:allo/logic/client/preferences/manager.dart';
 import 'package:allo/logic/client/preferences/preferences.dart';
 import 'package:allo/logic/core.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +12,7 @@ class SetupPersonalize extends HookConsumerWidget {
   const SetupPersonalize({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final dark = usePreference(ref, darkMode);
+    final dark = useSetting(ref, darkMode);
     void onSubmit() {
       Core.navigation.push(route: const SetupDone());
     }
