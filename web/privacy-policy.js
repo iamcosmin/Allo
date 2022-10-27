31a282d9e97c3f11cd34bc8a4295cd1f07767b43
iamcosmin <47219753+iamcosmin@users.noreply.github.com>
2022-10-27 11:26:24 +0300

    1.3.5 (330)+5
    Bring changes made in production.

---
+++ web/privacy-policy.js
@@ -0,0 +1,12 @@
+/* The state of Allo, by Relays.
+
+PRIVACY POLICY
+
+Allo, provided by relays (an alias, not a real business), is an open source product made by iamcosmon as a way to experiment with modern chat apps. This app is nowhere ready for production or for any intensive using, except for user testing.
+
+Allo collects some user data, such as their real name, email address and a virtually chosen username for the sake of functionality. Allo does not share ANY information with third parties, and your information is kept safe inside a Google-powered database service: Firebase Firestore. Your information is end to end encrypted in the limits of Firebase Firestore encryption. Allo technicians may have access to the database, but only if requested by the user (issues or unexpected behavior).
+
+You cannot delete your information by yourself (yet), though you can email us at lilcosminbs@gmail.com to request deletion of your entire account. Sent messages will not be deleted, but your personal data will not be associated with them anymore (just the name you set).
+
+If you have any questions, do not hesitate to contact us at the provided emails.
+Thank you! */
